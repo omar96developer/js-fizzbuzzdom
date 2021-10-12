87d7d883157b4183a98b03ba041a9eb4fda83329
omar96developer <omakino@hotmail.it>
2021-10-12 20:45:09 +0200

    1.2

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
-/*
-  <div class="row g-0">
-    <div class="col">
-      <div class="ratio ratio-1x1 border ${bgColor}">
-        <div></div>
-      </div>
-    </div>
-  </div>
-*/
-
-const containerScacchiera = document.getElementById("container_scacchiera");
-
-for (let i = 0; i < 14; i++) {
-  
-  const row = document.createElement("div");
-  row.classList.add("row", "g-0");
-  //row.textContent = 'riga numero' + 1;
-  for (let x = 0; x < 7; x++) {
-      row.innerHTML += `<div class="col">
-                            <div class="ratio ratio-1x1 border">
-                                <div></div>
-                            </div>
-                        </div>`
+for (let i = 1; i < 101; i++){
+  if (i % 15 == 0) {
+    console.log("FizzBuzz");
+  } 
+  else if (i % 3 == 0) {
+    console.log("Fizz");
+  } 
+  else if (i % 5 == 0)  {
+    console.log("Buzz");
   }
-
-  containerScacchiera.append(row);
+  else {
+    console.log(i);
+  }
 }
